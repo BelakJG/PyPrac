67c099540b4d116450c285690ae58b08a0e01219
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-03-02 14:51:46 -0500

    Tkinter labels and compund tag

---
+++ tkinterLabel.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.geometry("800x600")
+root.title("Tkinter Label Demo")
+
+label = ttk.Label(
+    root,
+    text = "This is a label",
+    font = ("Helvetica", 14)
+)
+label.pack()
+
+photo = tk.PhotoImage(file = "./assets/python.png")
+photo_label = ttk.Label(
+    root,
+    image = photo,
+    padding = 5,
+    text = "Python",
+    compound = "top"
+)
+photo_label.pack()
+
+root.mainloop()
