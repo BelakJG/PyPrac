d5999a5eed542e9147f711d956e53afcddb46669
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-03-01 22:17:15 -0500

    Added app icon

--- tkinterHello.py
+++ tkinterHello.py
@@ -16,6 +16,12 @@ screen_height = root.winfo_screenheight()
 center_x = int(screen_width/2 - window_width/2)
 center_y = int(screen_height/2 - window_height/2)
 
-root.geometry(f"{window_height}x{window_height}+{center_x}+{center_y}")
+root.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
+
+try:
+    photo = tk.PhotoImage(file="./assets/pythontutorial-1.png")
+    root.iconphoto(False, photo)
+except tk.TclError:
+    print("Icon not found, using default.")
 
 root.mainloop()
