907130eb1b5dd5b571e0cfb5ecbd928fb227645f
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-03-01 22:01:57 -0500

    tkinter set window size and places in center of screen

--- tkinterHello.py
+++ tkinterHello.py
@@ -6,4 +6,16 @@ root = tk.Tk()
 message = tk.Label(root, text = "Hello, World!")
 message.pack()
 
+root.title("Hello, World!")
+
+window_width = 800
+window_height = 600
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+center_x = int(screen_width/2 - window_width/2)
+center_y = int(screen_height/2 - window_height/2)
+
+root.geometry(f"{window_height}x{window_height}+{center_x}+{center_y}")
+
 root.mainloop()
